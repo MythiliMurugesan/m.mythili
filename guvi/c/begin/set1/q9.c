1d55c5909c13cd49f019130301bade41624136f3
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-02-24 20:14:29 +0530

    q9.c

---
+++ guvi/c/begin/set1/q9.c
@@ -0,0 +1,14 @@
+    #include<stdio.h>
+    int main()
+    {
+    int n,k,j,s=0;
+    int n1,n2,n3,n4,n5;
+    scanf("%d%d",&n,&k);
+    scanf("%d%d%d%d%",&n1,&n2,&n3,&n4,&n5);
+    for(j=0;j<=k;j++)
+    {
+      s=n1+n2;
+    }
+    printf("%d",s);
+    return 0;
+    }
