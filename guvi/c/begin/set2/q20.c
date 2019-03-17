cfff23c25762e1e99f9c9a35994fa448a401ec41
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-03-17 18:56:46 +0530

    q20.c

---
+++ guvi/c/begin/set2/q20.c
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+    int main()
+    {
+    int n,i,m;
+    scanf("%d",&n);
+    for(i=1;i<=5;i++)
+    {
+        m=i*n;
+        printf("%d\t",m);
+    }
+    return 0;
+    }
