eb455ef014b5a503d1a699474ac6ca39cb354c20
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-04-07 14:42:29 +0530

    q30.c

---
+++ guvi/c/begin/set3/q30.c
@@ -0,0 +1,22 @@
+    #include<stdio.h>
+    #include<stdlib.h>
+    int main()
+    {
+    int hrs,hrs1,mins,mins1,sub,sub1;
+    scanf("%d\t%d",&hrs,&mins);
+    scanf("\n%d\t%d",&hrs1,&mins1);
+    sub=abs(hrs-hrs1);
+    sub1=abs(mins-mins1);
+    printf("\n%d\t%d",sub,sub1);
+    return 0;
+    }
+
+    
+    
+    
+   
+    
+
+  
+
+
