83d0ba183ddfd88e046d367b567168ee0f873a29
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-04-07 13:44:20 +0530

    q29.c

---
+++ guvi/c/begin/set3/q29.c
@@ -0,0 +1,11 @@
+    #include<stdio.h>
+    int main()
+    {
+    int n,hours,mins;
+    scanf("%d",&n);
+    hours=n/60;
+    mins=n%60;
+    printf("%d\t%d",hours,mins);
+    
+    return 0;
+    }
