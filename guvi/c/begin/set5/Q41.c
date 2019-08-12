d74fcc03502e83956701b795155f2528d60095df
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-08-12 16:18:03 +0530

    Q41.c

---
+++ guvi/c/begin/set5/Q41.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+    int main()
+    {
+    int k,i;
+    char s[100];
+    scanf("%s%d",&s,&k);
+    for(i=1;i<=k;i++)
+    {
+        printf("%s\n",s);
+    }
+    return 0;
+    }
