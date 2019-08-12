6fa29ec65459570b4808276fd75987d508f613d3
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-08-12 16:07:23 +0530

    q6.c

--- guvi/c/begin/set1/q6.c
+++ guvi/c/begin/set1/q6.c
@@ -3,11 +3,7 @@
     {
     int year;
     scanf("%d",&year);
-    if((year%400)==0)
-    printf("yes");
-    else if((year%100)==0)
-    printf("no");
-    else if((year%4)==0)
+    if((year%400)==0) && (year%4==0))
     printf("yes");
     else
     printf("no");
