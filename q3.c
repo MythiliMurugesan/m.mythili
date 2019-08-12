9ce15e67cf0b6fcba6e3e97c4ce75f943e792d9d
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-08-12 15:41:17 +0530

    q3.c

--- q3.c
+++ q3.c
@@ -2,10 +2,18 @@
 int main()
 {
 char c;
-scanf("%c",&c);
-if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+scanf("%s",&c);
+if(c!='a' && c!='z')
+{
+  printf("invalid");
+}
+else if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
 printf("vowel");
+}
 else
+{
 printf("consonant");
+}
 return 0;
 }
