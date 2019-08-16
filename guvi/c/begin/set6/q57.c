8d5a25146e5cb8cdeff36d3b45ce83a3af6b6f65
MythiliMurugesan <47531335+MythiliMurugesan@users.noreply.github.com>
2019-08-16 19:17:12 +0530

    q57.c

---
+++ guvi/c/begin/set6/q57.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,a[10],i,count=0;
+    scanf("%d%d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==k)
+        {
+            count=count+1;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
